8ecd6b8f1ccfb5214e4263eaf40ebcedbcd7ab6a
YassineEli <yassine101057eli@gmail.com>
2026-03-08 01:26:12 +0000

    Checkpoint 1: Project setup with basic PySide6 GUI

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout,QWidget
+
+
+
+# Set a title and size.
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Beat & Lyrics Generator")      # Title
+        self.setMinimumSize(400, 200)                       # Wimdow size (width/height)
+
+        # Create central widget and layout
+        central_widget = QWidget()                          # A central widget holds a vertical layout.
+        self.setCentralWidget(central_widget)
+        layout = QVBoxLayout(central_widget)
+
+        # Create buttons
+        self.btn_generate_beat = QPushButton("Generate Beat")
+        self.btn_generate_full = QPushButton("Generate Full Track")
+
+        # Add buttons to layout
+        layout.addWidget(self.btn_generate_beat)
+        layout.addWidget(self.btn_generate_full)
+
+        # Style buttons a bit
+        self.btn_generate_beat.setMinimumHeight(50)         # button size height
+        self.btn_generate_full.setMinimumHeight(50)
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
